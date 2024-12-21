6d4e9200ec8d6ef4e62dc27909f8d65165e2af4f
Kaushal Kumar <Kaushal.kr.kr@gmail.com>
2024-12-21 22:32:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 function firstChar(text) {
-  // your code here
+  // Remove leading and trailing spaces, then find the first non-space character
+  for (let i = 0; i < text.length; i++) {
+    if (text[i] !== ' ') {
+      return text[i];
+    }
+  }
+  return ''; // Return an empty string if no non-space character is found
 }
 
 // Do not change the code below
-//Uncomment the following line to show the prompt popup
-//const text = prompt("Enter text:");
+// Uncomment the following line to show the prompt popup
+// const text = prompt("Enter text:");
 alert(firstChar(text));
